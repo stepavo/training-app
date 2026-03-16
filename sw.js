fb13bc9acc5232527ba63810fe355b695cd49649
stepavo <stefano.pavoni@gmail.com>
2026-03-16 14:17:00 +0100

    Add service worker

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "training-v1";
+const FILES = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
